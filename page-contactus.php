95fda883650cec666ad19d8d8f35e563fbc6257c
FathanRohman <doctor.fathan@gmail.com>
2017-08-01 07:55:32 +0700

    add sampe mycorporate website

---
+++ page-contactus.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'shareds/head.php'; ?>
+<body>
+	<div class="body--wrapper">
+		<?php include 'shareds/navbar.php'; ?>
+
+		<?php include 'shareds/intermezo-footer.php'; ?>
+		<?php include 'shareds/footer.php'; ?>
+	</div>
+</body>
+<?php include 'shareds/javascript.php'; ?>
+</html>
---
+++ shareds/javascript.php
@@ -0,0 +1,5 @@
+<!-- JAVASCRIPT -->
+<script src="assets/js/plugin/bootstrap.min.js"></script>
+<script src="assets/js/plugin/jquery.scrolltofixed.js"></script>
+<script src="assets/js/main.js"></script>
+<!-- /.JAVASCRIPT -->
