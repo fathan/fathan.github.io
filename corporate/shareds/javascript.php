c504ee3b9d71a3dde0b8e4764fc7f2302f563d8c
FathanRohman <doctor.fathan@gmail.com>
2017-08-06 22:52:44 +0700

    revision to vue.js corporate

--- corporate/shareds/javascript.php
+++
@@ -1,5 +0,0 @@
-<!-- JAVASCRIPT -->
-<script src="assets/js/plugin/bootstrap.min.js"></script>
-<script src="assets/js/plugin/jquery.scrolltofixed.js"></script>
-<script src="assets/js/main.js"></script>
-<!-- /.JAVASCRIPT -->
